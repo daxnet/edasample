f7b6ba1aa50c6f80f6aad997ac55c18bcd2d4077
daxnet <daxnet@outlook.com>
2019-09-09 21:47:10 +0800

    Adding saga to order service.

---
+++ src/services/EdaSample.Services.Orders/Sagas/CreateOrderSagaState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdaSample.Services.Orders.Sagas
+{
+    public class CreateOrderSagaState : ISagaState
+    {
+        public CreateOrderSagaState() => Id = Guid.NewGuid();
+
+        public Guid OrderId { get; set; }
+
+        public bool CustomerCreditAccepted { get; set; }
+
+        public bool InventoryAmountAccepted { get; set; }
+
+        public Guid Id { get; }
+    }
+}
---
+++ src/services/EdaSample.Services.Orders/Sagas/ISagaState.cs
@@ -0,0 +1,12 @@
+﻿using EdaSample.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdaSample.Services.Orders.Sagas
+{
+    public interface ISagaState : IAggregateRoot
+    {
+    }
+}
